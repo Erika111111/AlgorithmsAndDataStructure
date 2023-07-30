5b28f62aa14dba506803d70d0e532a0c68584d25
Erika <Erika1111@yandex.ru>
2023-07-30 14:49:20 +0300

    homework

---
+++ LinkedList.java
@@ -0,0 +1,61 @@
+public class LinkedList {
+    Node head;
+
+    static class Node{
+        int data;
+        Node prev;
+        Node next;
+
+
+        Node (int d){
+            data = d;
+            prev = null;
+            next = null;
+
+        }
+    }
+    
+
+    void reverse(){
+        Node temp = null;
+        Node current = head;
+
+        while(current != null){
+            temp = current.prev;
+            current.prev = current.next;
+            current.next = temp;
+            current = current.prev;
+        }
+
+        if(temp != null){
+            head = temp.prev;
+        }
+    }
+
+    void printList(Node node){
+        while(node != null){
+            System.out.println(node.data + " ");
+            node = node.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList list= new LinkedList();
+        list.head = new Node(1);
+        list.head.next = new Node(2);
+        list.head.next.prev = list.head;
+        list.head.next.next = new Node(3);
+        list.head.next.next.prev = list.head.next;
+        list.head.next.next.next = new Node(4);
+        list.head.next.next.next.prev = list.head.next.next;
+
+
+        System.out.println("Original linked list");
+        list.printList(list.head);
+
+        list.reverse();
+        System.out.println(" ");
+        System.out.println("reversed linked list ");
+        list.printList(list.head);
+    }
+}
